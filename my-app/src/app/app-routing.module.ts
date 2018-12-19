9d153d8d209c1dd4d8bd04361ab4307c9144223c
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2018-12-19 13:42:22 +0100

    route-heroes

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import {AppComponent} from './app.component';
 
+const routes: Routes=[
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  exports: [RouterModule]
+  exports: [RouterModule],
+  imports: [ RouterModule.forRoot(routes) ]
 })
 export class AppRoutingModule { }
